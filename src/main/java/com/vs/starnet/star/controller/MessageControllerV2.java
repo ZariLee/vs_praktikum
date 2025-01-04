2710df889002cc3acc3e8aa20741bf812c88ccb5
Larissa Pychlau <larissa.pychlau@haw-hamburg.de>
2025-01-04 16:55:42 +0100

    4/22 documentation done

--- src/main/java/com/vs/starnet/star/controller/MessageControllerV2.java
+++ src/main/java/com/vs/starnet/star/controller/MessageControllerV2.java
@@ -8,16 +8,23 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 /**
+ * newer version of the message controller
  */
 @RestController
 @RequestMapping("/vs/v2/messages")
 public class MessageControllerV2 {
     private final MessageService messageService;
 
+    // Connection to message service
     public MessageControllerV2(MessageService messageService) {
         this.messageService = messageService;
     }
 
+    /**
+     * message object is passed to the message service method
+     * @param message message Object containing all the details of the msg such as sender, star, content, etc
+     * @return response entity containing message's details.
+     */
     @PostMapping()
     public ResponseEntity<Map<String, String>> handleMessage(
             @RequestBody @Valid Message message) {
